50a79677ac03e5a2b3d27445c7718662b201759a
Winser Espinal <winserespinal@Winsers-MacBook-Pro.local>
2022-08-07 11:26:49 -0400

    Table component created without conneciton to a data source

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './app'
 import './index.css'
 
-const name = 'winser';
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, {name}</h1>
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+
