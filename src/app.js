0774926cc48f882532a09730cd0dc83ed3184ed4
Winser Espinal <winserespinal@Winsers-MacBook-Pro.local>
2022-08-07 21:21:22 -0400

    Props implemented

--- src/app.js
+++ src/app.js
@@ -2,26 +2,29 @@ import React, { Component } from 'react'
 import Table from "./Table";
 
 const name = 'Winser';
-class App extends React.Component {
+class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
     return (
-      <div className="App">
-        <h1>
-          Hello, my name is {name} and I'm learning how to use React.
-        </h1>
-        <h3>
-          JSX, important things to keep in mind:
-        </h3>
-        <p>
-          <ul>
-            <li>className is used instead of class for adding CSS classes, as class is a reserved keyword in JavaScript.</li>
-            <li>Properties and methods in JSX are camelCase - onclick will become onClick.</li>
-            <li>Self-closing tags must end in a slash.</li>
-            <li>JSX is easier to write and understand than creating and appending many elements in vanilla JavaScript, and is one of the reasons people love React so much.</li>
-          </ul>
-        </p>
-        <h3></h3>
-        <Table />
+      <div className="container">
+        <Table characterData={characters} />
       </div>
     )
   }
