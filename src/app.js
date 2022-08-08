42bc5f0194ba5de3f08e88df46cff927d969f1ab
Winser Espinal <winserespinal@Winsers-MacBook-Pro.local>
2022-08-07 22:50:24 -0400

    Done with core app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Table from "./Table";
+import Form from './Form'
 
 const name = 'Winser';
 class App extends Component {
   state = {
     characters: [
-      
+
     ],
   }
 
@@ -19,11 +20,16 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
     const { characters } = this.state
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit}/>
       </div>
     )
   }
